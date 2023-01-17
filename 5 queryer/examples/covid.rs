bb38936455bd21c7dc6f85ea16a0b0f06c9421e2
CD Yang <cdyang1991@gmail.com>
2023-01-17 17:41:53 +0800

    queryer demo

---
+++ 5 queryer/examples/covid.rs
@@ -0,0 +1,41 @@
+use anyhow::{Ok, Result};
+use queryer::query;
+
+// use polars::prelude::*;
+// use std::io::Cursor;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
+
+    let sql = format!(
+        "SELECT location name, total_cases, new_cases, total_deaths \
+        FROM {} where new_deaths >= 500 ORDER BY new_cases DESC",
+        url
+    );
+    let df1 = query(sql).await?;
+    print!("{:?}", df1);
+
+    // let data = reqwest::get(url).await?.text().await?;
+
+    // // 使用 polars 直接请求
+    // let df = CsvReader::new(Cursor::new(data))
+    //     .infer_schema(Some(16))
+    //     .finish()?;
+
+    // let filtered = df.filter(&df["new_deaths"].gt(500))?;
+    // println!(
+    //     "{:?}",
+    //     filtered.select((
+    //         "location",
+    //         "total_cases",
+    //         "new_cases",
+    //         "total_deaths",
+    //         "new_deaths"
+    //     ))
+    // );
+
+    Ok(())
+}
